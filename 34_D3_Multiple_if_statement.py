15110987a638d0155cb3084ea9bce3149e8fe9b1
AHPranto <pranto258@gmail.com>
2023-02-12 04:08:03 +0600

    added 45 and 55 ages

--- 34_D3_Multiple_if_statement.py
+++ 34_D3_Multiple_if_statement.py
@@ -1,6 +1,7 @@
 # if height avobe 120cm then you can ride otherwise you cant
 # if your age > 12 then pay 5,if age <= 10 then pay 7, else pay 12
 # Whants photo? bill +3
+# 45-55 ages people are free ride
 print("Welcome to the Roller Coaster")
 
 height = int(input("What is your height in cm? "))
@@ -15,6 +16,8 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Please pay 7 tk")
+    elif age >=45 and age<=55:
+        print("Have a Free ride on us!")
     else:
         bill = 12
         print("Please pay 12 tk")
