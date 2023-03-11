5c3738876a1dd2e95e1e16f49e02a74fa54d3340
AHPranto <pranto258@gmail.com>
2023-03-11 16:45:30 +0600

    added turtle work

--- 170_D18_GenerateRandomWalk.py
+++ 170_D18_GenerateRandomWalk.py
@@ -2,14 +2,24 @@ import random
 import turtle as t
 
 tim = t.Turtle()
+t.colormode(255)
 
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
+#1 colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 # East(0),North(90),West(180),South(270)
 directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
 for _ in range(200):
-    tim.color(random.choice(colours))
+    #1 tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
